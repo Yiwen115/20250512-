55c7e2c9dc4e7357b0ee301f54a957bdc611aaa1
Yiwen115 <yiwen951127@gmail.com>
2025-05-12 11:29:29 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let facemesh;
 let video;
 let predictions = [];
 const lipPoints = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+const leftEyePoints = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155];
+const rightEyePoints = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249];
 
 function setup() {
   createCanvas(640, 480); // 創建畫布
@@ -31,6 +33,7 @@ function draw() {
   pop();
 
   drawLips(); // 繪製嘴唇
+  drawEyes(); // 繪製眼睛
 }
 
 function drawLips() {
@@ -50,5 +53,36 @@ function drawLips() {
       }
     }
     endShape(CLOSE); // 將最後一點與第一點連接
+  }
+}
+
+function drawEyes() {
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh; // 獲取臉部特徵點
+
+    stroke(255, 0, 0); // 紅色線條
+    strokeWeight(3); // 線條粗細為3
+
+    // 繪製左眼
+    for (let i = 0; i < leftEyePoints.length - 1; i++) {
+      const [x1, y1] = keypoints[leftEyePoints[i]];
+      const [x2, y2] = keypoints[leftEyePoints[i + 1]];
+      line(x1, y1, x2, y2); // 繪製兩點之間的線
+    }
+    // 將左眼最後一點與第一點連接
+    const [lx1, ly1] = keypoints[leftEyePoints[0]];
+    const [lx2, ly2] = keypoints[leftEyePoints[leftEyePoints.length - 1]];
+    line(lx1, ly1, lx2, ly2);
+
+    // 繪製右眼
+    for (let i = 0; i < rightEyePoints.length - 1; i++) {
+      const [x1, y1] = keypoints[rightEyePoints[i]];
+      const [x2, y2] = keypoints[rightEyePoints[i + 1]];
+      line(x1, y1, x2, y2); // 繪製兩點之間的線
+    }
+    // 將右眼最後一點與第一點連接
+    const [rx1, ry1] = keypoints[rightEyePoints[0]];
+    const [rx2, ry2] = keypoints[rightEyePoints[rightEyePoints.length - 1]];
+    line(rx1, ry1, rx2, ry2);
   }
 }

ffd26dc55bfaa9e0f6a0ccfedbd20b511eb06965
Yiwen115 <yiwen951127@gmail.com>
2025-05-12 10:35:03 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let predictions = [];
 const lipPoints = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480); // 調整畫布大小
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(width, height); // 設置與畫布相同大小
   video.hide();
 
   facemesh = ml5.facemesh(video, modelReady);
@@ -20,7 +20,8 @@ function modelReady() {
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
+  background(0); // 黑色背景
+  image(video, 0, 0, width, height); // 顯示影像
   drawLips();
 }
 
@@ -35,8 +36,8 @@ function drawLips() {
     beginShape();
     for (let i = 0; i < lipPoints.length; i++) {
       const pointIndex = lipPoints[i];
-      const [x, y] = keypoints[pointIndex];
-      vertex(x, y);
+      const [x, y] = keypoints[pointIndex]; // 獲取嘴唇點的座標
+      vertex(x, y); // 在嘴唇點繪製頂點
     }
     endShape(CLOSE); // 將最後一點與第一點連接
   }
